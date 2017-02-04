e50cffa00e15b4adca4723d69d5a781e0a709a54
Greg Adams <gta@oddish.cs.uchicago.edu>
2017-02-04 13:15:35 -0600

    setup bash file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# setup.sh
+# Downloads all the files I used for analysis.
+
+
+git clone https://github.com/gregorytadams/BA.git
+cd BA
+git clone https://github.com/propublica/compas-analysis.git
+
+# At the end, run "rm -rf BA" from the command line to delete the files this downloads.
